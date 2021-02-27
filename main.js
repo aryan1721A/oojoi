b3f40b90ea8c259ade5c522ca9aeafbdb9b148e7
Aryan sonthalia <75519498+aryan1721A@users.noreply.github.com>
2021-02-27 16:24:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+}
+function setup(){
+    canvas=creatCanvas(300,300);
+    canvas.centre();
+}
+
+function draw(){
+}
+
+function take_snapshot(){
+    sav("myfilterimage.png");
+}
